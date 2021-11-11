3708f39fd9bec0c8b64265c975e4fbd5c94801ac
ElisaOppe <91607662+ElisaOppe@users.noreply.github.com>
2021-11-11 12:17:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n=0;
+    int n1=0, n2=1, n3=n1+n2;
+
+    cin>>n;
+
+    if(n>=2){
+        cout << n2 << endl;
+        for(int i=3;i<=(n+1);i++) {
+            cout << n3 << endl;
+            n1=n2;
+            n2=n3;
+            n3=n1+n2;
+        }
+
+    }else{
+        cout << "Errore";
+    }
+
+    return 0;
 }
+
